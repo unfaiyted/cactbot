66b7238d6fa297bb274d04c7566de7afae153745
Miller, Dane (PLV4343) <dane.miller@usaa.com>
2018-09-28 12:40:18 -0500

    adjustments to timline bars to have custom limits and proper grammer

--- ui/settings/settings.js
+++ ui/settings/settings.js
@@ -12,22 +12,48 @@ $('.volume').slider({
 });
 
 
-$('.slider').slider({
-  min: 0,
-  max: 20,
-  value: 0,
-  range: 'min',
-  slide: function(e, ui) {
-    setVolume(ui.value / 100);
-    $(e.target).siblings('.slider-val').text(ui.value + 'seconds');
-  },
+
+$('.slider').each( function (index, value) {
+    let that = $(this);
+
+
+    that.slider({
+             min: that.attr('data-min'),
+             max: that.attr('data-max'),
+             value: 0,
+             range: 'min',
+             slide: function(e, ui) {
+               setVolume(ui.value / 100);
+               $(e.target).siblings('.slider-val').text(
+               ui.value + ' ' + that.attr('data-unit') +
+                ((ui.value > 1 || ui.value === 0) ? 's': ''));
+             },
+     });
 });
+
 
 
 $('h2').click((e) => {
   const nextDiv = $(e.target.parentNode).next('div');
-  $('#cactbot-module section .header + div').removeClass('active');
-  nextDiv.addClass('active');
+
+    let subSections =   $('#cactbot-module section .header + div');
+    let headers = $('#cactbot-module section h2');
+
+  if(nextDiv.hasClass('active')) {
+    nextDiv.removeClass('active');
+    $(e.target).removeClass('active');
+    subSections.removeClass('active');
+    headers.removeClass('active');
+  } else {
+      subSections.removeClass('active');
+      headers.removeClass('active');
+      $(e.target).addClass('active');
+      nextDiv.addClass('active');
+  }
+
+
+
+
 });
 
 $('.menu span').click((e) => {
